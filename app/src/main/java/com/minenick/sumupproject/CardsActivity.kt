d1257d3e4bf12c0bf837346d4bc7688b1f866f2b
minenick121 <nicolas_12_acuario@hotmail.com>
2021-10-20 00:00:44 -0500

    90% terminado los recycles View, falta implementar un poco de bases de datos para obtener los datos de las tarjetas

--- app/src/main/java/com/minenick/sumupproject/CardsActivity.kt
+++ app/src/main/java/com/minenick/sumupproject/CardsActivity.kt
@@ -2,15 +2,26 @@ package com.minenick.sumupproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.minenick.sumupproject.adapter.RecyclerAdapter
 import com.minenick.sumupproject.databinding.ActivityAuthBinding
+import com.minenick.sumupproject.databinding.ActivityCardsBinding
 
 class CardsActivity : AppCompatActivity() {
     private val email:String?=intent.extras?.getString("email")
-    private lateinit var binding:ActivityAuthBinding
+    private lateinit var db:FirebaseFirestore
+    private lateinit var binding:ActivityCardsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityAuthBinding.inflate(layoutInflater)
+        binding= ActivityCardsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initRecycle()
 
+    }
+
+    private fun initRecycle() {
+        binding.rvCard.layoutManager=LinearLayoutManager(this)
+        val adapter= RecyclerAdapter()
+        binding.rvCard.adapter=adapter
     }
 }
--- app/src/main/java/com/minenick/sumupproject/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/minenick/sumupproject/adapter/RecyclerAdapter.kt
@@ -1,4 +1,43 @@
 package com.minenick.sumupproject.adapter
 
-class RecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.minenick.sumupproject.R
+import com.minenick.sumupproject.databinding.ItemListViewBinding
+import com.minenick.sumupproject.entities.Card
+import com.squareup.picasso.Picasso
+
+class RecyclerAdapter (private val cardList:ArrayList<Card>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val binding=ItemListViewBinding.bind(itemView)
+        fun render(card:Card){
+            binding.tvNroCard.text=card.number
+            binding.tvPropietario.text=card.owner
+            binding.tvFecha.text="${card.date?.year} / ${card.date?.month}"
+            Picasso.get().load(card.img).into(binding.ivCard)
+            itemView.setOnClickListener {
+                Toast.makeText(itemView.context,"Tarjeta elegida : ${card.number}",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v= LayoutInflater.from(parent.context).inflate(R.layout.item_list_view,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.render(cardList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return cardList.size
+    }
 }
